1be2345ec27d85e28440b0b8667e7655c46e03d9
bandhansaha2 <bandhansaha2@gmail.com>
2024-11-27 03:35:18 +0600

    Week 5 Day 2

---
+++ Week 5/Day 2/Minimum_XOR.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while(t--) {
+        int n, XOR = 0; cin >> n;
+        vector <int> v(n);
+        for(int i=0; i<n; i++) {
+            cin >> v[i];
+            XOR ^= v[i];
+        }
+        int ans = XOR;
+        for(int i=0; i<n; i++) {
+            int curXor = XOR ^ v[i];
+            ans =  min (ans,curXor);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
