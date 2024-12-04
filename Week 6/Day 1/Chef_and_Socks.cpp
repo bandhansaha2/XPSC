bdab50a06a300177c3a02f04b0e4e2cac3eeb6d4
bandhansaha2 <bandhansaha2@gmail.com>
2024-12-04 23:59:46 +0600

    Week 6 Day 1

---
+++ Week 6/Day 1/Chef_and_Socks.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int x,y,z;
+    cin >> x >> y >> z;
+
+    if(y+z >= x) Yes;
+    else No;
+
+    return 0;
+}
